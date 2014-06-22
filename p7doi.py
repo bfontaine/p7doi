b832787453457cffbc432e357eea9c4d96c434f9
Baptiste Fontaine <batifon@yahoo.fr>
2014-06-22 17:54:55 +0200

    initial code

---
+++ p7doi.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+import sys
+import os
+
+DOI_URL = 'http://rproxy.sc.univ-paris-diderot.fr/login?url=http://dx.doi.org/%s'
+
+# note: to be behind P7's proxy, transform an URL like this:
+#   http://domain.tld/path
+# into:
+#   http://domain.tld.rproxy.sc.univ-paris-diderot.fr/path
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print 'usage: %s <doi>' % sys.argv[0]
+        sys.exit(1)
+
+    doi = sys.argv[1]
+
+    os.system("open '%s'" % (DOI_URL % doi,))
