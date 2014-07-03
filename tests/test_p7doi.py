acc3276e6667b3cfc666b8539261fb87526369ed
Baptiste Fontaine <batifon@yahoo.fr>
2014-07-03 20:24:54 +0200

    Missing tests.

--- tests/test_p7doi.py
+++ tests/test_p7doi.py
@@ -3,13 +3,15 @@
 import platform
 
 if platform.python_version() < '2.7':
+    from StringIO import StringIO
     import unittest2 as unittest
 else:
+    from io import StringIO
     import unittest
 
-import sys
 import webbrowser
 import p7doi
+from p7doi import sys
 
 class TestP7doi(unittest.TestCase):
 
@@ -18,12 +20,24 @@ class TestP7doi(unittest.TestCase):
         def fake_open_new_tab(*args):
             self.wb_args = args
 
+        self.exit_status = None
+        def fake_exit(s):
+            self.exit_status = s
+
         self._open_new_tab = webbrowser.open_new_tab
         self._argv = sys.argv
+        self._stdout = sys.stdout
+        self._exit = sys.exit
+        self._version = p7doi.__version__
+        sys.stdout = StringIO()
+        sys.exit = fake_exit
         webbrowser.open_new_tab = fake_open_new_tab
 
     def tearDown(self):
         sys.argv = self._argv
+        sys.stdout = self._stdout
+        sys.exit = self._exit
+        p7doi.__version__ = self._version
         webbrowser.open_new_tab = self._open_new_tab
 
     # __version__
@@ -61,3 +75,41 @@ class TestP7doi(unittest.TestCase):
         self.assertSequenceEqual([
             'http://rproxy.sc.univ-paris-diderot.fr/login?url=http://dx.doi.org/abcd-1234'
         ], self.wb_args)
+
+    def test_cli_not_enough_args(self):
+        sys.argv = ['xyz']
+        p7doi.cli()
+        sys.stdout.seek(0)
+        self.assertSequenceEqual('Usage: xyz <doi>\n', sys.stdout.read())
+        self.assertEquals(1, self.exit_status)
+
+    def test_cli_version_short_flag(self):
+        sys.argv = ['xyz', '-v']
+        p7doi.__version__ = 'foobar42'
+        p7doi.cli()
+        sys.stdout.seek(0)
+        self.assertSequenceEqual('p7doi vfoobar42\n', sys.stdout.read())
+        self.assertEquals(0, self.exit_status)
+
+    def test_cli_version_long_flag1(self):
+        sys.argv = ['xyz', '-version']
+        p7doi.__version__ = 'f42'
+        p7doi.cli()
+        sys.stdout.seek(0)
+        self.assertSequenceEqual('p7doi vf42\n', sys.stdout.read())
+        self.assertEquals(0, self.exit_status)
+
+    def test_cli_version_long_flag2(self):
+        sys.argv = ['xyz', '--version']
+        p7doi.__version__ = '1xyz'
+        p7doi.cli()
+        sys.stdout.seek(0)
+        self.assertSequenceEqual('p7doi v1xyz\n', sys.stdout.read())
+        self.assertEquals(0, self.exit_status)
+
+    def test_cli_version_unrecognized_option(self):
+        sys.argv = ['xyz', '-x']
+        p7doi.cli()
+        sys.stdout.seek(0)
+        self.assertSequenceEqual("Unrecognized option: '-x'\n", sys.stdout.read())
+        self.assertEquals(1, self.exit_status)
